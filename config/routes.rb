47fea1f9f76dbc06999ac6083b1779a9cdc0d25a
Allen Kim <avalmercafe02@gmail.com>
2023-03-09 02:16:27 -0500

    commit tree 8ab2d8990a543c0f057f54f642bdd29d6a24bbe9

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create]
+  resources :birds, only: [:index, :show, :create, :update]
+   def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+  patch "/birds/:id/like", to: "birds#increment_likes"
 end
